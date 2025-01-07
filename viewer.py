955f72e87d102364595d3817788ac6f9b246bdb0
Abdullah-hmed <abdullahwyi@gmail.com>
2025-01-07 23:20:21 +0500

    removed progress bar for gifs

--- viewer.py
+++ viewer.py
@@ -1,5 +1,6 @@
 from PIL import Image
-import os, time, tqdm
+import os, time, shutil
+import numpy as np
 import imageio as iio
 import argparse
 
@@ -8,12 +9,19 @@ ascii_chars = ' ▁▂▃▄▅▆▇█'
 
 def image_to_unicode(frame, new_width=80):
     
+    terminal_width, terminal_height = shutil.get_terminal_size()
+    
     img = Image.fromarray(frame).convert("L")
 
-    # Resize image
+    # Resize image to fit terminal dimensions
     width, height = img.size
     aspect_ratio = height / width
+    if new_width is None:
+        new_width = terminal_width
     new_height = int(new_width * aspect_ratio * 0.55)
+    if new_height > terminal_height:
+        new_height = terminal_height
+        new_width = int(new_height / (aspect_ratio * 0.55))
     resized_img = img.resize((new_width, new_height))
 
     # Map pixels to unicode
@@ -25,16 +33,20 @@ def image_to_unicode(frame, new_width=80):
     return ascii_img
 
 def video_to_unicode(video_path, width=80, frame_rate=24):
-    frame_count = iio.get_reader(video_path).count_frames()
     
-    video_reader = iio.imiter(video_path)
-    current_frame = 0
+    
+    video_reader = iio.get_reader(video_path)
+    if not video_path.endswith('.gif'):
+        frame_count = video_reader.count_frames()
+    
+    current_frame = 1
     for frame in video_reader:
         unicode_frame = image_to_unicode(frame, new_width=width)
         
         os.system('cls' if os.name == 'nt' else 'clear')
-        print('\r', unicode_frame)
-        progress_bar(current_frame, frame_count, width)
+        print('\r', unicode_frame, end='', flush=True)
+        if not video_path.endswith('.gif'):
+            progress_bar(current_frame, frame_count, width)
         current_frame += 1
         time.sleep(1 / frame_rate)
 
@@ -59,7 +71,9 @@ if __name__ == "__main__":
         if args.video:
             video_to_unicode(args.input_path, width=args.width)
         else:
-            ascii_art = image_to_unicode(args.input_path, args.width)
+            img = Image.open(args.input_path)
+            frame = np.array(img)
+            ascii_art = image_to_unicode(frame, args.width)
             print(ascii_art)
     except FileNotFoundError:
         print(f"Error: File '{args.input_path}' not found.")
